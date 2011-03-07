d6b7f741f63240576d211736be8db7a264b7164b
Jonathan Mackenzie <jonmac1@gmail.com>
2011-03-08 01:18:57 +1030

    fixed dataN disappearance

--- parser.py
+++ parser.py
@@ -10,15 +10,15 @@ import plugins.scroll
 from heapq import merge
 
 def parse(conn):
-    #print (conn.dataN['fool']+' '+conn.dataN['chan']+': '+conn.dataN['msg'])
+    print (conn.dataN['fool']+' '+conn.dataN['chan']+': '+conn.dataN['msg'])
     if conn.dataN['cmd'] == 'KICK' and nick in conn.dataN['raw']:
         del (conn.chans[conn.dataN['chan']]) 
     if conn.dataN['cmd'] == 'PRIVMSG' and len(conn.dataN['words']) != 0:
         # Run the function for the given command            
         if conn.dataN['fool'] in conn.admins:
-            conn.dataN = check(list(merge(pluginList, adminPlugins)),conn)
+            check(list(merge(pluginList, adminPlugins)),conn)
         else:
-            conn.dataN = check(pluginList,conn)
+            check(pluginList,conn)
 
 def check(pl,conn):
     for plugin in pl:
