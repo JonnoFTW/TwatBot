c80dd1306cb2e03a2a46b64105a8ab91352e513d
Jonathan Mackenzie <jonno@9ch.in>
2011-04-03 19:07:50 +0930

    updated plugins

--- plugins/help.py
+++ plugins/help.py
@@ -1,4 +1,4 @@
-help ="Send the the line preceeding ^^ to @Buttsworth_ on twitter. List all cmds with ^cmds, specific help with ^xyz help .http://twitter.com/#!/Buttsworth_"
+help ="Send the the line preceeding ^^ to @Buttsworth_ on twitter. List all cmds with ^cmds, specific help with ^xyz help . http://twitter.com/#!/Buttsworth_ Source code available at: https://github.com/JonnoFTW/TwatBot"
 
 def shelp(conn):
     conn.sendMsg(help,conn.dataN['chan'])
