45c83b7a06a872d7f1b522640685deb41e9f057d
Jonathan Mackenzie <jonno@9ch.in>
2011-04-09 02:53:54 +0930

    post counter should be faster

--- plugins/checkem.py
+++ plugins/checkem.py
@@ -1,13 +1,28 @@
 import urllib
 import re
-help = "4chan dubs getter AIO. use ^check <board> for current post no. of a bord . 9MB Bale pack at http://www.mediafire.com/?j3idvabga74e8sy"
+help = "4chan dubs getter AIO. use ^check <board> for current post no. of a board . 9MB Bale pack at http://www.mediafire.com/?j3idvabga74e8sy"
+
+def readWord(f,s = ''):
+    p = f.read(1)
+    if p == ' ':
+        return s
+    else:
+        return readWord(f,s+p)
+def getNos(f):
+    nos = []
+    # Read in first 8 tags
+    count = 0
+    while count != 8:
+       word = readWord(f)
+       if word[:6] == 'id=\"no':
+         count+= 1
+         nos.append(re.sub("[^0-9]","",word))
+    return max(nos)
+
 def getPostCount(board):
    url = 'http://boards.4chan.org/'+board
    f = urllib.urlopen(url)
-   page = f.read()
-   #Filter out the post numbers
-   no = max(map(lambda x: re.sub("[^0-9]","",x),filter(lambda x: (x[:6] == 'id=\"no'),page.split())))
-   return no
+   return getNos(f)
 boards = ['a','b','c','d','e','f','g','gif','h','hr','k','m','o','p','r','s','t','u','v','w','wg','i','ic','cm','y','3','adv','an','cgl','ck','co','fa','fit','int','jp','lit','mu','n','po','sci','soc','sp','tg','toy','trv','tv','vp','x']
 def getNo(conn):
     if len(conn.dataN['words']) > 1:
@@ -15,7 +30,7 @@ def getNo(conn):
         if chan not in boards:
             conn.sendMsg('Please select an actual channel',conn.dataN['chan'])
         else:
-            conn.sendMsg('Current post for '+chan+' is:'+getPostCount(chan),conn.dataN['chan'])    
+            conn.sendMsg('Current post for '+chan+' is: '+getPostCount(chan),conn.dataN['chan'])    
     else:
         conn.sendMsg('How about you give me a board?',conn.dataN['chan'])
  
