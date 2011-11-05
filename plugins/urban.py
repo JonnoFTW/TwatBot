7648e0eb72a798732f88ee7d6897d86d4f367e60
Jonathan Mackenzie <jonmac1@gmail.com>
2011-11-05 22:53:54 +1030

    fixed bugs in urban and google plugins, they should probably be merged

--- plugins/urban.py
+++ plugins/urban.py
@@ -4,11 +4,11 @@ help = "Gets the definition of a word from the urbandictionary website"
 
 def urban(conn):
   try:
-    d = json.load(urllib2.urlopen("http://www.urbandictionary.com/iphone/search/define?term="+conn.dataN['words'][1]))
+    d = json.load(urllib2.urlopen("http://www.urbandictionary.com/iphone/search/define?term="+('+'.join(conn.dataN['words'][1:]))))
     if 'pages' not in d:
       conn.sendMsg("Word is not defined")
     else:
-      conn.sendMsg(d['list'][0]['definition'])
+      conn.sendMsg(d['list'][0]['definition']+'--- '+d['list'][0]['example'])
   except Exception, e:
     print e
     conn.sendMsg("Usage is: ^ud <word>")
